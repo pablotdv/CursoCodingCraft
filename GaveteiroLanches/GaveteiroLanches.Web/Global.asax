b37cf02093017e55d00d8e1123c5dcaa630b412f
Pablo Tôndolo de Vargas <pablotdv@gmail.com>
2016-06-16 14:02:34 -0300

    Estrutura do projeto

---
+++ GaveteiroLanches/GaveteiroLanches.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GaveteiroLanches.Web.MvcApplication" Language="C#" %>
---
+++ GaveteiroLanches/GaveteiroLanches.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GaveteiroLanches.Web.Startup))]
+namespace GaveteiroLanches.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
