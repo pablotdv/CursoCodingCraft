684b70faecd7f058e974144de1263526bafb05e7
Pablo Tôndolo de Vargas <pablotdv@gmail.com>
2016-07-01 16:12:00 -0300

    Primeiro commit

---
+++ Exercicio02Layouts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Exercicio02Layouts.MvcApplication" Language="C#" %>
---
+++ Exercicio02Layouts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Exercicio02Layouts.Startup))]
+namespace Exercicio02Layouts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
