0b0bd7e97819b41fb8e5d7e1853e7c01c3e11180
Pablo Tôndolo de Vargas <pablotdv@gmail.com>
2016-07-25 13:42:27 -0300

    Adicionado os Scaffold

---
+++ Exercicio10Cep/Exercicio10Cep/CodeTemplates/Scaffolders/Portugues.RazorView/Portugues.RazorView.ps1
@@ -0,0 +1,65 @@
+[T4Scaffolding.Scaffolder(Description = "Adds ASP.NET MVC views for Create/Read/Update/Delete/Index scenarios")][CmdletBinding()]
+param(        
+	[parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)][string]$Controller,
+	[string]$ModelType,
+	[string]$Area,
+	[string]$ViewName,
+	[string]$Template,
+	[alias("MasterPage")]$Layout = "",	# If not set, we'll use the default layout
+ 	[alias("ContentPlaceholderIDs")][string[]]$SectionNames,
+	[alias("PrimaryContentPlaceholderID")][string]$PrimarySectionName,
+	[switch]$ReferenceScriptLibraries = $false,
+    [string]$Project,
+	[string]$CodeLanguage,
+	[string[]]$TemplateFolders,
+	[string]$ViewScaffolder = "View",
+	[switch]$Force = $false
+)
+
+if($ViewName -eq "Index" -or !$ViewName) {
+	@("Index") | %{
+		Scaffold $ViewScaffolder -Controller $Controller -ViewName $_ -ModelType $ModelType -Template $_ -Area $Area -Layout $Layout -SectionNames $SectionNames -PrimarySectionName $PrimarySectionName -ReferenceScriptLibraries:$ReferenceScriptLibraries -Project $Project -CodeLanguage $CodeLanguage -OverrideTemplateFolders $TemplateFolders -Force:$Force
+	}
+
+	if ($Template -eq "bootstrap" -or !$Template) {
+		@("Index_bootstrap") | %{
+			Scaffold $ViewScaffolder -Controller $Controller -ViewName $_ -ModelType $ModelType -Template $_ -Area $Area -Layout $Layout -SectionNames $SectionNames -PrimarySectionName $PrimarySectionName -ReferenceScriptLibraries:$ReferenceScriptLibraries -Project $Project -CodeLanguage $CodeLanguage -OverrideTemplateFolders $TemplateFolders -Force:$Force
+		}
+	}
+
+	if ($Template -eq "foundation" -or !$Template) {
+		@("Index_foundation") | %{
+			Scaffold $ViewScaffolder -Controller $Controller -ViewName $_ -ModelType $ModelType -Template $_ -Area $Area -Layout $Layout -SectionNames $SectionNames -PrimarySectionName $PrimarySectionName -ReferenceScriptLibraries:$ReferenceScriptLibraries -Project $Project -CodeLanguage $CodeLanguage -OverrideTemplateFolders $TemplateFolders -Force:$Force
+		}
+	}
+
+	if ($Template -eq "Ink" -or !$Template) {
+		@("Index_Ink") | %{
+			Scaffold $ViewScaffolder -Controller $Controller -ViewName $_ -ModelType $ModelType -Template $_ -Area $Area -Layout $Layout -SectionNames $SectionNames -PrimarySectionName $PrimarySectionName -ReferenceScriptLibraries:$ReferenceScriptLibraries -Project $Project -CodeLanguage $CodeLanguage -OverrideTemplateFolders $TemplateFolders -Force:$Force
+		}
+	}
+}
+
+if($ViewName -eq "Criar" -or !$ViewName) {
+	@("Criar") | %{
+			Scaffold $ViewScaffolder -Controller $Controller -ViewName $_ -ModelType $ModelType -Template $_ -Area $Area -Layout $Layout -SectionNames $SectionNames -PrimarySectionName $PrimarySectionName -ReferenceScriptLibraries:$ReferenceScriptLibraries -Project $Project -CodeLanguage $CodeLanguage -OverrideTemplateFolders $TemplateFolders -Force:$Force
+		}
+
+	if ($Template -eq "bootstrap" -or !$Template) {
+		@("Criar_bootstrap", "_CriarOuEditar_bootstrap") | %{
+			Scaffold $ViewScaffolder -Controller $Controller -ViewName $_ -ModelType $ModelType -Template $_ -Area $Area -Layout $Layout -SectionNames $SectionNames -PrimarySectionName $PrimarySectionName -ReferenceScriptLibraries:$ReferenceScriptLibraries -Project $Project -CodeLanguage $CodeLanguage -OverrideTemplateFolders $TemplateFolders -Force:$Force
+		}
+	}
+
+	if ($Template -eq "foundation" -or !$Template) {
+		@("Criar_foundation", "_CriarOuEditar_foundation") | %{
+			Scaffold $ViewScaffolder -Controller $Controller -ViewName $_ -ModelType $ModelType -Template $_ -Area $Area -Layout $Layout -SectionNames $SectionNames -PrimarySectionName $PrimarySectionName -ReferenceScriptLibraries:$ReferenceScriptLibraries -Project $Project -CodeLanguage $CodeLanguage -OverrideTemplateFolders $TemplateFolders -Force:$Force
+		}
+	}
+
+	if ($Template -eq "Ink" -or !$Template) {
+		@("Criar_Ink", "_CriarOuEditar_Ink") | %{
+			Scaffold $ViewScaffolder -Controller $Controller -ViewName $_ -ModelType $ModelType -Template $_ -Area $Area -Layout $Layout -SectionNames $SectionNames -PrimarySectionName $PrimarySectionName -ReferenceScriptLibraries:$ReferenceScriptLibraries -Project $Project -CodeLanguage $CodeLanguage -OverrideTemplateFolders $TemplateFolders -Force:$Force
+		}
+	}
+}
